c0deea7c951db38fd7577e39dd7bd03c8b9a69ba
andela-jare <jorg.are@andela.com>
2017-01-16 11:21:46 +0100

    feature(embed-disqus): add disqus as a comment platform Finishes #137005169

--- imports/plugins/included/product-detail-simple/client/templates/productDetailSimple.js
+++ imports/plugins/included/product-detail-simple/client/templates/productDetailSimple.js
@@ -9,3 +9,12 @@ Template.productDetailSimple.helpers({
     return ProductDetailContainer;
   }
 });
+Template.disqus.helpers({
+  getDisqus() {
+    const d = document;
+    const s = d.createElement("script");
+    s.src = "//anbu-rc.disqus.com/embed.js";
+    s.setAttribute("data-timestamp", +new Date());
+    (d.head || d.body).appendChild(s);
+  }
+});
