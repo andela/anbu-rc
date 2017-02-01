113575892e1f2ca8ae83fe7c021ec822f601b1b5
andela-jare <jorg.are@andela.com>
2017-02-01 16:36:31 +0100

    bug(houndci): fix houndci violations Finishes #137005173

--- server/publications/collections/static-pages.js
+++ server/publications/collections/static-pages.js
@@ -1,4 +1,6 @@
+import { Meteor } from "meteor/meteor";
 import { StaticPages } from "/lib/collections";
+
 Meteor.publish("staticPages", () => {
   if (this.userId === null) {
     return this.ready();
